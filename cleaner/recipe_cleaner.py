9bcc4e41cb153ebd79c1d69c9daef2375da93ad6
Ermias Bizuwork <ermiasbizuwork@ermiass-air.lan>
2020-05-19 21:20:26 -0400

    finished dropping columns

--- cleaner/recipe_cleaner.py
+++ cleaner/recipe_cleaner.py
@@ -3,10 +3,6 @@ import us
 
 # read csv
 df = pd.read_csv('data/recipe/epirecipes/epi_r.csv')
-# print(df.columns[:30])
-states = [us.states.STATES]
-print(type(states[0]))
-
 
 
 def drop_columns(df, columns=[]):
@@ -20,11 +16,26 @@ def drop_columns(df, columns=[]):
     :param columns: (optional)
     :return:
     """
-    states = us.states.STATES
-    cols = []
+    def create_cols(cols=[]):
+        states = [s.name.lower() for s in us.states.STATES]
+        return cols+states
+
+    def drop_the_columns(df, cols, ind):
+        df = df.drop(index=ind)
+        for c in cols:
+            try:
+                df = df.drop(columns=c)
+            except Exception as e:
+                print(e)
+                continue
+        return df
+    # create some stuff to use
+    cols_to_drop = create_cols()
     indices_to_drop = range(1,13)
-    # df.columns
 
-    df.drop(columns=cols, index=indices_to_drop)
+    # drop the columns
+    df = drop_the_columns(df, cols_to_drop, indices_to_drop)
+    return df
 
+df = drop_columns(df)
 
