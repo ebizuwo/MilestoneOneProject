a6c343162247c0e6b3996bc896402123f48399ac
Ermias Bizuwork <ermias_bizuwork@homedepot.com>
2020-05-22 14:53:00 -0400

    updated recipe cleaner slightly

--- cleaner/recipe_cleaner.py
+++ cleaner/recipe_cleaner.py
@@ -1,8 +1,22 @@
 import pandas as pd
 import us
+import holidays
 
 # read csv
 df = pd.read_csv('data/recipe/epirecipes/epi_r.csv')
+
+class column_collector(object):
+    outputs = []
+    def __init__(self, f):
+        self.f = f()
+        print(self.f)
+        try:
+            self.outputs.extend(self.f)
+        except Exception as e:
+            print(e)
+
+    def __call__(self):
+        pass
 
 
 def drop_columns(df, columns=[]):
@@ -17,11 +31,27 @@ def drop_columns(df, columns=[]):
     """
 
 
-    def create_cols(cols=[]):
-        # we add the states in here as well
+    @column_collector
+    def input_cols():
+        if len(columns)>0:
+            return columns
+        else:
+            return None
+
+    @column_collector
+    def create_states():
         states = [s.name.lower() for s in us.states.STATES]
-        cities = []
-        return cols+states
+        return states
+
+    @column_collector
+    def create_cities():
+        return None
+
+    @column_collector
+    def create_holidays():
+        us_holidays = holidays.UnitedStates(years=2020).values()
+        return [h.lower() for h in us_holidays]
+
 
     def drop_the_columns(df, cols, ind):
         df = df.drop(index=ind)
@@ -33,7 +63,9 @@ def drop_columns(df, columns=[]):
                 continue
         return df
     # create some stuff to use
-    cols_to_drop = create_cols(cols=columns)
+    cols_to_drop = column_collector.outputs
+    print(cols_to_drop)
+    # cols_to_drop = create_cols(cols=columns)
     indices_to_drop = range(1,13)
 
     # drop the columns
@@ -41,6 +73,9 @@ def drop_columns(df, columns=[]):
     return df
 
 
+def write_file(df, f):
+    df.to_csv(f)
 
 df = drop_columns(df)
-print(list(df.columns))
+columns_list = list(df.columns)
+print(columns_list)
